1d2d4ec5c14715b76066c28cd4cb57a0c8dd2f56
Paul Renato <prenato@gmail.com>
2016-03-07 23:31:23 -0800

    Update win-dns.ps1

--- win-dns.ps1
+++ win-dns.ps1
@@ -1,3 +1,1 @@
-#Write-Host "Hello, world"
-#Read-Host "Press Enter to continue"
 Add-DnsServerResourceRecordA -Name "Baca3" -ZoneName "CNSGTMLAB.local" -AllowUpdateAny -IPv4Address "10.20.10.13" -TimeToLive 01:00:00
